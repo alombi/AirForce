269031c23ac28e24f329c9b91e14e3fe3155dd79
alombi <alombi@hotmail.com>
2021-04-19 14:56:37 +0200

    Reorganization

---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<main>
+	<h1 class="title">AirForce</h1>
+	<p>Work in progress. The project is open source at <a href="https://github.com/alombi/airforce">https://github.com/alombi/airforce</a></p>
+</main>
