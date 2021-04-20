87c18978d764eb04fb7c44be40bd7e8b88288b8f
alombi <alombi@hotmail.com>
2021-04-20 13:50:15 +0200

    Improves features section

--- src/components/Features.svelte
+++ src/components/Features.svelte
@@ -1,14 +1,29 @@
 <div class="features">
    <div class="element">
       <ion-icon id="flash" name="flash"></ion-icon>
-      <h2>Performant</h2>
+      <div class="desc">
+         <h2>Performant</h2>
+         <p>AirForce is fast and easy to use</p>
+      </div>
    </div>
    <div class="element">
       <ion-icon id="plane" name="airplane"></ion-icon>
-      <h2>Powerful</h2>
+      <div class="desc">
+         <h2>Powerful</h2>
+         <p>Tracks flights worldwide in a spark</p>
+      </div>
    </div>
    <div class="element">
       <ion-icon id="planet" name="planet"></ion-icon>
-      <h2>Elegant</h2>
+      <div class="desc">
+         <h2>Elegant</h2>
+         <p>AirForce features clean and nice UIs</p>
+      </div>
    </div>
 </div>
+
+<style>
+   p{
+      margin-top:-17px;
+   }
+</style>
